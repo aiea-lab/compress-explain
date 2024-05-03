94ce87f8acf5c6078448909419136cd786252f5d
Coen Adler <co41co@gmail.com>
2024-05-02 21:20:27 -0700

    prune remove old weights

--- pruning.py
+++ pruning.py
@@ -5,5 +5,7 @@ def vanilla_prune(model, conv_prune=0.3, linear_prune=0.6):
     for name, module in model.named_modules():
         if isinstance(module, torch.nn.Conv2d):
             prune.l1_unstructured(module, name='weight', amount=conv_prune)
+            prune.remove(module, 'weight')
         elif isinstance(module, torch.nn.Linear):
             prune.l1_unstructured(module, name='weight', amount=linear_prune)
+            prune.remove(module, 'weight')
