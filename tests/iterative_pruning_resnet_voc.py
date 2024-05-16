234662fc0ed9e034270523a849555f8f0a88c39e
Coen Adler <co41co@gmail.com>
2024-05-16 16:05:26 -0700

    iterative pruning 10 epochs

--- tests/iterative_pruning_resnet_voc.py
+++ tests/iterative_pruning_resnet_voc.py
@@ -176,7 +176,7 @@ class VocModel(nn.Module):
 
 model = VocModel(num_classes=20, pretrained=False).to(device)
 model.load_state_dict(torch.load('../saved_models/resnet34_pretrain_best_9.pt'))
-epochs = 5
+epochs = 10
 max_lr = 0.001
 grad_clip = 0.1
 weight_decay = 1e-4
